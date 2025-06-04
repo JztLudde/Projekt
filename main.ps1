0457eb26046dfe7048f5202a3673f6729f367fc4
Ludvig <ludvig.svensson.student@ksgyf.se>
2025-06-04 17:44:52 +0200

    La till try & if

--- main.ps1
+++ main.ps1
@@ -1,1 +1,12 @@
 Import-Module ".\SystemCheck.psm1"
+
+try {
+    $usage = Get-SystemUsage
+    $disk = Get-DiskSpace
+
+    Write-Log -Message "CPU Usage: $($usage.CPUUsage)% - Free Memory: $($usage.FreeMemoryGB)GB" -Path $logPath
+    Write-Log -Message "Disk Space: $($disk.FreeSpaceGB)GB" -Path $logPath
+
+      if ($usage.CPUUsage -gt $thresholdCPU) {
+        Write-Warning "CPU-användningen är hög: $($usage.CPUUsage)%"
+    }
