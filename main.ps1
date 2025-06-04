1a5f50bc59cc80c8dffc9be843c06bae37443be5
Ludvig <ludvig.svensson.student@ksgyf.se>
2025-06-04 18:07:18 +0200

    La till if för Minne och IF för Hårddiskutrymme samt catch ifall fel uppstår

--- main.ps1
+++ main.ps1
@@ -10,3 +10,15 @@ try {
       if ($usage.CPUUsage -gt $thresholdCPU) {
         Write-Warning "CPU-användningen är hög: $($usage.CPUUsage)%"
     }
+
+      if ($usage.FreeMemoryGB -lt $thresholdMemory) {
+        Write-Warning "Lite minne kvar: $($usage.FreeMemoryGB)GB"
+    }
+
+    if ($disk.FreeSpaceGB -lt $thresholdDiskSpace) {
+        Write-Warning "Lite utrymme kvar på disk: $($disk.FreeSpaceGB)GB"
+    }
+}
+catch {
+    Write-Error "Ett fel inträffade: $_"
+}
