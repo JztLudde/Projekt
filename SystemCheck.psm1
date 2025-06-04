fcaebc1d6ce39dc8d4dd3b8be02fcff54da0439d
Kevin Kosner <kevin.kosner.student@ksgyf.se>
2025-06-04 18:05:48 +0200

    SystemCheck.psm1 grundkod färdig, måste testa efter main.ps1 är klar

--- SystemCheck.psm1
+++ SystemCheck.psm1
@@ -1,6 +1,12 @@
 function Get-SystemUsage {
     try {
-
+        $cpu = Get-CimInstance Win32_Processor | Select-Object -ExpandProperty LoadPercentage
+        $memory = Get-CimInstance Win32_OperatingSystem | 
+            Select-Object @{Name="FreeMemoryGB"; Expression={[math]::Round($_.FreePhysicalMemory/1MB,2)}}
+        
+        return [PSCustomObject]@{
+            CPUUsage = $cpu
+            FreeMemoryGB = $memory.FreeMemoryGB
     }
 }
 catch {
